107076baf68b0790236dc8f7b8f4b8b9820b9d6e
Farid Mammadov <faridmmmdv@gmail.com>
2016-11-29 20:50:44 +0900

    Create day15.java

---
+++ 30DaysOfCode/day15.java
@@ -0,0 +1,10 @@
+    public static  Node insert(Node head,int data) {
+        if (head == null)
+            return new Node(data);
+        else if (head.next == null)
+            head.next = new Node(data);
+        else
+            insert(head.next,data);
+
+        return head;
+    }
