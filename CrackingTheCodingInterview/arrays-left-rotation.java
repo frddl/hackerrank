9701118e5eb1fe5749d7989da5e5a336c890d6cf
Farid Mammadov <faridmmmdv@gmail.com>
2016-11-29 21:06:38 +0900

    Create arrays-left-rotation.java

---
+++ CrackingTheCodingInterview/arrays-left-rotation.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static int[] arrayLeftRotation(int[] a, int n, int k) {
+      int[] res = a;
+       
+      for (int i = 0; i < k; i++){
+          int temp = res[0];
+          for (int j = 0; j < n - 1; j++){
+              res[j] = res[j+1];
+          }
+          
+          res[res.length-1] = temp;
+      }
+        
+        return res;
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+      
+        int[] output = new int[n];
+        output = arrayLeftRotation(a, n, k);
+        for(int i = 0; i < n; i++)
+            System.out.print(output[i] + " ");
+      
+        System.out.println();
+      
+    }
+}
