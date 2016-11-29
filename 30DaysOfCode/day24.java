546bc8ec1e8822afbdcf7225d0b020971786f45f
Farid Mammadov <faridmmmdv@gmail.com>
2016-11-29 20:58:17 +0900

    Create day24.java

---
+++ 30DaysOfCode/day24.java
@@ -0,0 +1,12 @@
+public static Node removeDuplicates(Node head) {
+  if (head == null || head.next == null)
+    return head;
+    
+  if (head.data == head.next.data){
+    head.next = head.next.next;
+    removeDuplicates(head);
+  } else 
+    removeDuplicates(head.next);
+      
+  return head;
+}
