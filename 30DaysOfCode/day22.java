01f33fac753904c0967e3885cd041d382a821f71
Farid Mammadov <faridmmmdv@gmail.com>
2016-11-29 20:54:55 +0900

    Update day22.java

--- 30DaysOfCode/day22.java
+++ 30DaysOfCode/day22.java
@@ -1,4 +1,4 @@
-	public static int getHeight(Node root){
+public static int getHeight(Node root){
         if (root == null) return -1;
-        return Math.max(getHeight(root.left),getHeight(root.right)) + 1;
-    }
+	return Math.max(getHeight(root.left),getHeight(root.right)) + 1;
+}
