74f5c5933285db4da58ff4540bf75cdf9f3a83b3
Farid Mammadov <faridmmmdv@gmail.com>
2016-11-29 22:49:55 +0900

    Create army-game.java
    https://www.hackerrank.com/contests/w26/challenges/game-with-cells/

---
+++ army-game.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        System.out.print((n/2 + n%2) * (m/2 + m%2));
+    }
+}
