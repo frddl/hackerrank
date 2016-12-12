617fd18805eaaac75ebcaf66c99002ea659ad1d0
Farid Mammadov <faridmmmdv@gmail.com>
2016-12-12 22:08:52 +0900

    Create weather-observation-station-5.sql

---
+++ SQL/weather-observation-station-5.sql
@@ -0,0 +1,3 @@
+select * from (select city, length(city) from station order by length(city) asc, city asc limit 1) t1
+union all
+select * from (select city, length(city) from station order by length(city) desc, city asc limit 1) t2;
